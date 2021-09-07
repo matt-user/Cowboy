bcc812a06ea22e22bc0bf59d67d901bf09ae63e7
Matt Auer <mattauer@umich.edu>
2021-09-07 18:39:18 -0400

    added compile script

---
+++ ethereum/compile.js
@@ -0,0 +1,41 @@
+// Compiles Contracts and outputs them to a json abi
+const path = require('path');
+const solc = require('solc');
+const fs = require('fs-extra');
+
+const buildPath = path.resolve(__dirname, 'build');
+// Whenever we compile/recompile remove the abis
+fs.removeSync(buildPath);
+
+// Compile the solidity contract file and output the abi
+const contractFolder = 'contracts';
+const contractName = 'Battle.sol';
+const contractPath = path.resolve(__dirname, contractFolder, contractName);
+const source = fs.readFileSync(contractPath, 'utf-8');
+const input = {
+    language: "Solidity",
+    sources: {
+        contractName: {
+            content: source,
+        },
+    },
+    settings: {
+        outputSelection: {
+            "*": {
+                "*": ["*"],
+            },
+        },
+    },
+};
+
+const output = JSON.parse(solc.compile(JSON.stringify(input))).contracts[contractName];
+
+// Create build folder if it does not already exist
+fs.ensureDirSync(buildPath);
+
+for (let contract in output) {
+    fs.outputJSONSync(
+        path.resolve(buildPath, `${contract}.json`),
+        output[contract]
+    );
+}
