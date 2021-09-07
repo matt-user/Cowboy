eb9833fcdce99b575ce7c223abc8dccc1680311d
Matt Auer <mattauer@umich.edu>
2021-09-07 16:58:07 -0400

    cowboy battling

---
+++ ethereum/Battle.sol
@@ -0,0 +1,95 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract BattleHandler {
+    struct Battle {
+        Cowboy cowboy1;
+        Cowboy cowboy2;
+        string winner;
+        uint turnCounter;
+        bool gameOver;
+    }
+
+    struct Cowboy {
+        string name;
+        uint shots;
+        bool takenTurn;
+        bool reloading;
+        bool shooting;
+    }
+
+    Battle battle;
+
+    constructor() {
+        Cowboy memory cowboy1 = Cowboy("uno", 0, false, false, false);
+        Cowboy memory cowboy2 = Cowboy("dos", 0, false, false, false);
+        battle = Battle(cowboy1, cowboy2, "", 0, false);
+    }
+
+    function getWinner() public view returns(string memory) {
+        require(battle.gameOver, "There is no winner yet.");
+        return battle.winner;
+    }
+
+    function takeTurn(uint command, uint cowboyId) public {
+        require(!battle.gameOver, "The game is over your cowboy can't take turns.");
+        Cowboy storage cowboy;
+        if (cowboyId == 1){
+            cowboy = battle.cowboy1;
+        } else {
+            cowboy = battle.cowboy2;
+        }
+        require(!cowboy.takenTurn, "Cowboy has already taken turn this round");
+
+        if (command == 0) {
+            reload(cowboy);
+        } else if (command == 1) {
+            shoot(cowboy);
+        } else if (command == 2) {
+            dodge(cowboy);
+        } else {
+            // Error
+        }
+        cowboy.takenTurn = true;
+        battle.turnCounter++;
+        if (battle.turnCounter == 2) {
+            // Both cowboys have taken their turn
+            finishTurn(battle.cowboy1, battle.cowboy2);
+        }
+    }
+
+    function finishTurn(Cowboy storage cowboy1, Cowboy storage cowboy2) internal {
+        if (cowboy1.shooting && cowboy2.reloading) {
+            // Cowboy 1 wins
+            battle.winner = cowboy1.name;
+            battle.gameOver = true;
+        } else if (cowboy2.shooting && cowboy1.reloading){
+            // Cowboy 2 wins
+            battle.winner = cowboy2.name;
+            battle.gameOver = true;
+        }
+        // Reset for next turn
+        cowboy1.takenTurn = false;
+        cowboy2.takenTurn = false;
+        battle.turnCounter = 0;
+    }
+
+    function reload(Cowboy storage cowboy) internal {
+        cowboy.shots++;
+        cowboy.reloading = true;
+        cowboy.shooting = false;
+    }
+
+    function shoot(Cowboy storage cowboy) internal {
+        require(cowboy.shots >= 1, "Your cowboy can't shoot without ammo");
+        cowboy.shots--;
+        cowboy.reloading = false;
+        cowboy.shooting = true;
+    }
+
+    function dodge(Cowboy storage cowboy) internal {
+        cowboy.reloading = false;
+        cowboy.shooting = false;
+    }
+}
--- ethereum/contracts/BlockchainCowboys.sol
+++ ethereum/contracts/BlockchainCowboys.sol
@@ -5,24 +5,37 @@ pragma solidity >=0.7.0 <0.9.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract BlockChainCowboys is ERC721{
+    struct BattleOffer {
+        bool wantsToBattle;
+        address willOnlyBattle;  // Specify to only battle a specific person
+    }
+
+    struct Battle {
+        uint cowboyId1;
+        uint cowboyId2;
+        uint turns;
+        string command1;
+        string command2;
+    }
+
     struct Cowboy {
         string name;
         uint shots;
+        bool takenTurn;
         bool dodging;
         bool shooting;
     }
 
     enum Commands{ RELOAD, SHOOT, DODGE }
 
+    // Maps the id of a cowboy to if they want to battle or not
+    mapping(uint => BattleOffer) wantsToBattle;
     Cowboy[] public cowboys;
-    Cowboy private cowboy1;
-    Cowboy private cowboy2;
+    Battle battle;
     address public gameOwner;
-    bool turnOver;
 
     constructor() {
         gameOwner = msg.sender;
-        turnOver = false;
     }
 
     modifier onlyOwnerOf(uint _cowboyId) {
@@ -30,32 +43,52 @@ contract BlockChainCowboys is ERC721{
         _;
     }
 
-    modifier onlyGameOwner(string memory message) {
-        require(gameOwner == msg.sender, message);
+    modifier onlyGameOwner() {
+        require(gameOwner == msg.sender, "Only the game owner can do this");
         _;
     }
 
-    function createNewCowboy(string memory _name, address _to) public onlyGameOwner("Only the game owner can create new cowboys.") {
+    function createNewCowboy(string memory _name, address _to) public onlyGameOwner {
         uint id = cowboys.length;
-        cowboys.push(Cowboy(_name, 0, false, false));
+        cowboys.push(Cowboy(_name, 0, false, false, false));
         _safeMint(_to, id);
     }
 
-    function startBattle(uint _cowboy1, uint _cowboy2) public onlyGameOwner("Only the game owner can start battles between cowboys.") {
-        cowboy1 = cowboys[_cowboy1];
-        cowboy2 = cowboys[_cowboy2];
+    /**
+    * @dev Offers a battle with the cowboy of the given id
+    * @param cowboyId id of cowboy who wants to fight
+    **/
+    function offerCowboyToBattle(uint cowboyId) public onlyOwnerOf(cowboyId) {
+        wantsToBattle[cowboyId] = BattleOffer(true, address(0));
     }
 
-    function takeTurn(uint command, uint cowboy) public onlyOwnerOf(cowboy) {
+    /**
+    * @dev Offers a battle with the cowboy specifically to the address to
+    * @param cowboyId id of cowboy who wants to fight to address caller wants to fight
+    **/
+    function offerCowboyToBattle(uint cowboyId, address to) public onlyOwnerOf(cowboyId) {
+        wantsToBattle[cowboyId] = BattleOffer(true, to);
+    }
+
+    function fightCowboy(uint ownerCowboyId, uint cowboyId) public onlyOwnerOf(ownerCowboyId) {
+        BattleOffer storage battleOffer = wantsToBattle[cowboyId];
+        require(battleOffer.wantsToBattle, "This cowboy does not want to battle right now.");
+        require(battleOffer.willOnlyBattle == address(0) || battleOffer.willOnlyBattle == msg.sender, "This cowboy does not want to battle with you");
+        battle = Battle(cowboyId, ownerCowboyId, 0, "", "");
+    }
+
+    function takeTurn(uint command, uint cowboyId) public onlyOwnerOf(cowboyId) {
+        require(!cowboys[cowboyId].takenTurn, "This cowboy cannot take a turn until this turn is over");
         if (command == 0) {
-            reload(cowboy);
+            reload(cowboyId);
         } else if (command == 1) {
-            shoot(cowboy);
+            shoot(cowboyId);
         } else if (command == 2) {
-            dodge(cowboy);
+            dodge(cowboyId);
         } else {
             // Error
         }
+        cowboys[cowboyId].takenTurn = true;
     }
 
     function reload(uint _cowboy) public {
