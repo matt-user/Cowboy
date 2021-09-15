e728be1fd7387ed5bf675d31ab03715db807a191
Matt Auer <mattauer@umich.edu>
2021-09-15 14:15:02 -0400

    feat: basic index page working

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Button } from 'semantic-ui-react';
+import battleHandler from '../ethereum/battleHandler';
+import Layout from '../components/Layout';
+
+class BattleIndex extends Component {
+    render() {
+        return <h1>Index</h1>;
+    }
+}
+
+export default BattleIndex;
