deded7d188fc5e18f07481d956b72a2682e22a8d
Matt Auer <mattauer@umich.edu>
2021-09-16 20:54:19 -0400

    chore: add layout component to index

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import { Button } from 'semantic-ui-react';
+import Link from 'next/link';
 import battleHandler from '../ethereum/battleHandler';
 import Layout from '../components/Layout';
 
 class BattleIndex extends Component {
     render() {
-        return <h1>Index</h1>;
+        return (
+            <Layout>
+                <h1>Battles</h1>
+                <Link href="/battles/new">
+                    <a><Button floated="right" content="Create a Battle" icon="add circle" primary /></a>
+                </Link>
+            </Layout>
+        );
     }
 }
 
